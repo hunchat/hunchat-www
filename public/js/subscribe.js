a7038b96fce596773944aecfb79ebbcb3d73cf48
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-02-08 15:32:41 +0000

    :children_crossing: add "wait" message to submit button in subscribe form

--- public/js/subscribe.js
+++ public/js/subscribe.js
@@ -3,6 +3,12 @@ Webflow.push(function() {
   const failMessage = $('#wf-form-Subscribe-Form-fail');
 
   $('#wf-form-Subscribe-Form').submit(function(event) {
+    var btn = $('input[type="submit"]');
+    var label = btn.attr('value')
+    btn.prop('disabled', true); // disable submit button
+    var wait = btn.attr('data-wait')
+    btn.val(wait) // set button value to "Please wait..."
+
     var formData = {
       'email': $('input[name=email]').val(),
     };
@@ -15,10 +21,19 @@ Webflow.push(function() {
     })
       .done(function(data) {
         successMessage.toggle();
+
+        // Reset submit button
+        btn.prop('disabled', false); // enable submit button
+        btn.val(label) // set button value to "Gain access"
       })
       .fail(function(data) {
         failMessage.toggle();
+
+        // Reset submit button
+        btn.prop('disabled', false); // enable submit button
+        btn.val(label) // set button value to "Gain access"
       })
+
     event.preventDefault();
     return false;
   });
